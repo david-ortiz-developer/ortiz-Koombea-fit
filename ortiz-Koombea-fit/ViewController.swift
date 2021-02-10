03e03cf83aba5e3668c5f556a3f7da9be1bd97a7
davit <david.ortiz.developer@gmail.com>
2021-02-10 15:14:47 -0500

    Initial Commit

---
+++ ortiz-Koombea-fit/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ortiz-Koombea-fit
+//
+//  Created by edgar david ortiz diaz on 10/02/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ ortiz-Koombea-fitTests/ortiz_Koombea_fitTests.swift
@@ -0,0 +1,33 @@
+//
+//  ortiz_Koombea_fitTests.swift
+//  ortiz-Koombea-fitTests
+//
+//  Created by edgar david ortiz diaz on 10/02/21.
+//
+
+import XCTest
+@testable import ortiz_Koombea_fit
+
+class ortiz_Koombea_fitTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
